b3c7c5999488c48889796ef32674a3b6202a7678
jarabey <jasminarabeysantos@gmail.com>
2024-08-06 19:48:43 -0600

    Index Translation

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ document.getElementById('translate-btn').addEventListener('click', function() {
     translatePage();
 });
 
+var check=documen.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked;
+    if(id==true){
+        location.href="es/index.html";
+    }else{
+        location.href="../index.html";
+    }
+}
